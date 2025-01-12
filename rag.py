4afa54d5bf3acd44680b35f2334c8ed044395fc6
Anusha Lihala <anusha.lihala@koneksys.com>
2025-01-12 21:02:12 +0530

    Update RAG: Add react and compact mode

--- rag.py
+++ rag.py
@@ -1,13 +1,20 @@
 import os
-from llama_index.core import SimpleDirectoryReader
+from llama_index.core import SimpleDirectoryReader, Settings
 from llama_index.core.node_parser import SentenceSplitter
-from llama_index.core import Settings
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import SummaryIndex, VectorStoreIndex, Document
 from llama_index.core.tools import QueryEngineTool
-from llama_index.core.agent import FunctionCallingAgentWorker
-from llama_index.core.agent import ParallelAgentRunner
+from llama_index.core.agent import (
+    FunctionCallingAgentWorker,
+    ParallelAgentRunner,
+    ReActAgent,
+)
+from llama_index.core.query_engine import MultiStepQueryEngine, RetrieverQueryEngine
+from llama_index.core.indices.query.query_transform.base import (
+    StepDecomposeQueryTransform,
+)
+
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -17,6 +24,7 @@ class RAGAgent:
         self.llm = OpenAI(model="gpt-4o-mini")
         Settings.llm = self.llm
         Settings.embed_model = OpenAIEmbedding(model="text-embedding-ada-002")
+        self.job_desc = job_desc
 
         # load documents
         documents = SimpleDirectoryReader(input_files=[cv_file_path]).load_data()
@@ -34,9 +42,15 @@ class RAGAgent:
         self._get_summary_tools()
 
     def _get_vector_tools(self):
+        step_decompose_transform = StepDecomposeQueryTransform(
+            llm=self.llm, verbose=True
+        )
+
         if self.cv_vector_tool is None:
             cv_vector_index = VectorStoreIndex(self.cv_nodes)
-            cv_vector_query_engine = cv_vector_index.as_query_engine()
+            cv_vector_query_engine = RetrieverQueryEngine.from_args(
+                retriever=cv_vector_index.as_retriever(), response_mode="compact"
+            )
             self.cv_vector_tool = QueryEngineTool.from_defaults(
                 name="cv_vector_tool",
                 query_engine=cv_vector_query_engine,
@@ -44,10 +58,24 @@ class RAGAgent:
                     "Useful for retrieving specific context about user's CV/resume."
                 ),
             )
+            cv_vector_ms_query_engine = MultiStepQueryEngine(
+                query_engine=cv_vector_query_engine,
+                query_transform=step_decompose_transform,
+                index_summary="Useful for retrieving specific context about user's CV/resume",
+            )
+            self.cv_vector_ms_tool = QueryEngineTool.from_defaults(
+                name="cv_vector_ms_tool",
+                query_engine=cv_vector_ms_query_engine,
+                description=(
+                    "Useful for retrieving specific context about user's CV/resume."
+                ),
+            )
 
         if self.job_vector_tool is None:
             job_vector_index = VectorStoreIndex(self.job_nodes)
-            job_vector_query_engine = job_vector_index.as_query_engine()
+            job_vector_query_engine = RetrieverQueryEngine.from_args(
+                retriever=job_vector_index.as_retriever(), response_mode="compact"
+            )
             self.job_vector_tool = QueryEngineTool.from_defaults(
                 name="job_vector_tool",
                 query_engine=job_vector_query_engine,
@@ -55,8 +83,24 @@ class RAGAgent:
                     "Useful for retrieving specific context about job posting user is applying to."
                 ),
             )
+            job_vector_ms_query_engine = MultiStepQueryEngine(
+                query_engine=job_vector_query_engine,
+                query_transform=step_decompose_transform,
+                index_summary="Useful for retrieving specific context about job posting user is applying to.",
+            )
+            self.job_vector_ms_tool = QueryEngineTool.from_defaults(
+                name="job_vector_ms_tool",
+                query_engine=job_vector_ms_query_engine,
+                description=(
+                    "Useful for retrieving specific context about job posting user is applying to."
+                ),
+            )
 
     def _get_summary_tools(self):
+        step_decompose_transform = StepDecomposeQueryTransform(
+            llm=self.llm, verbose=True
+        )
+
         if self.cv_summary_tool is None:
             cv_summary_index = SummaryIndex(self.cv_nodes)
             cv_summary_query_engine = cv_summary_index.as_query_engine(
@@ -66,6 +110,18 @@ class RAGAgent:
             self.cv_summary_tool = QueryEngineTool.from_defaults(
                 name="cv_summary_tool",
                 query_engine=cv_summary_query_engine,
+                description=(
+                    "Useful for summarization questions related to user's CV/resume."
+                ),
+            )
+            cv_summary_ms_query_engine = MultiStepQueryEngine(
+                query_engine=cv_summary_query_engine,
+                query_transform=step_decompose_transform,
+                index_summary="Useful for summarization questions related to user's CV/resume.",
+            )
+            self.cv_summary_ms_tool = QueryEngineTool.from_defaults(
+                name="cv_summary_ms_tool",
+                query_engine=cv_summary_ms_query_engine,
                 description=(
                     "Useful for summarization questions related to user's CV/resume."
                 ),
@@ -84,23 +140,73 @@ class RAGAgent:
                     "Useful for summarization questions related to job posting user is applying to."
                 ),
             )
+            job_summary_ms_query_engine = MultiStepQueryEngine(
+                query_engine=job_summary_query_engine,
+                query_transform=step_decompose_transform,
+                index_summary="Useful for summarization questions related to job posting user is applying to.",
+            )
+            self.job_summary_ms_tool = QueryEngineTool.from_defaults(
+                name="job_summary_ms_tool",
+                query_engine=job_summary_ms_query_engine,
+                description=(
+                    "Useful for summarization questions related to job posting user is applying to."
+                ),
+            )
 
-    def _auto_rag(self, prompt):
-        agent_worker = FunctionCallingAgentWorker.from_tools(
-            [
-                self.cv_vector_tool,
-                self.job_vector_tool,
-                self.cv_summary_tool,
-                self.job_summary_tool,
-            ],
-            llm=self.llm,
-            verbose=True,
+    def _get_response_from_react(self, agent, prompt):
+        task = agent.create_task(prompt)
+        step_output = agent.run_step(task.task_id)
+        while not step_output.is_last:
+            step_output = agent.run_step(task.task_id)
+        response = agent.finalize_response(task.task_id)
+
+        for step in task.extra_state["current_reasoning"]:
+            print(step)
+
+        thoughts = [
+            step.thought
+            for step in task.extra_state["current_reasoning"]
+            if hasattr(step, "thought")
+        ]
+        prev_two_thoughts = "\n".join(thoughts[-2:])
+        response = self.llm.complete(
+            f"""
+        Extract the tailored CV from the text below.
+        Provide only the tailored CV and no other text:
+                                    
+        {prev_two_thoughts}
+        {response}
+        """
         )
-        agent = ParallelAgentRunner(agent_worker)
-        response = agent.query(prompt)
         return str(response)
 
-    def _guided_rag(self, beginning_prompt):
+    def _auto_rag(self, prompt, use_react=False):
+        if use_react:
+            agent = ReActAgent.from_tools(
+                [self.cv_vector_ms_tool, self.cv_summary_ms_tool],
+                llm=self.llm,
+                verbose=True,
+                context=f"{self.job_desc}",
+            )
+            prompt = """Tailor the user's CV to be the best possible fit for the job description (provided as context). Do not invent information not in the user's CV.
+                """
+            return self._get_response_from_react(agent, prompt)
+        else:
+            agent_worker = FunctionCallingAgentWorker.from_tools(
+                [
+                    self.cv_vector_tool,
+                    self.job_vector_tool,
+                    self.cv_summary_tool,
+                    self.job_summary_tool,
+                ],
+                llm=self.llm,
+                verbose=True,
+            )
+            agent = ParallelAgentRunner(agent_worker)
+            response = agent.query(prompt)
+            return str(response)
+
+    def _guided_rag(self, beginning_prompt, use_react=False):
         job_vector_query_engine = self.job_vector_tool.query_engine
         hard_skills = job_vector_query_engine.query(
             "What are the hard skills and tool knowledge required for this position?"
@@ -109,7 +215,7 @@ class RAGAgent:
             "What are the soft skills required for this position?"
         )
         quals = job_vector_query_engine.query(
-            "What are the qualifications required for this position?"
+            "What are the degrees required for this position?"
         )
 
         cv_vector_query_engine = self.cv_vector_tool.query_engine
@@ -120,55 +226,59 @@ class RAGAgent:
         cvsum = self.cv_summary_tool.query_engine.query("cv summary")
         jobsum = self.job_summary_tool.query_engine.query("job summary")
 
-        agent_worker = FunctionCallingAgentWorker.from_tools(
-            [
-                self.cv_vector_tool,
-                self.job_vector_tool,
-                self.cv_summary_tool,
-                self.job_summary_tool,
-            ],
-            llm=self.llm,
-            verbose=True,
-        )
-        agent = ParallelAgentRunner(agent_worker)
-        response = agent.query(
-            f"""{beginning_prompt}
+        prompt = f"""{beginning_prompt}
 
-        CV summary:
-        {str(cvsum)}
+            CV summary:
+            {str(cvsum)}
 
-        Job description summary:
-        {str(jobsum)}
+            Job description summary:
+            {str(jobsum)}
 
-        Emphasize the following:
-        {str(hard_skills_evid)}
+            Emphasize the following:
+            {str(hard_skills_evid)}
 
-        {str(soft_skills_evid)}
+            {str(soft_skills_evid)}
 
-        {str(quals_evid)}
+            {str(quals_evid)}"""
 
-        Use the available tools for any other information you need.
-        Specific questions about the CV can be answered using cv_vector_tool and specific questions about the job can be answered using job_vector_tool.
-        """
-        )
-        return str(response)
+        if use_react:
+            agent = ReActAgent.from_tools(
+                [self.cv_vector_ms_tool, self.cv_summary_ms_tool],
+                llm=self.llm,
+                verbose=True,
+                context=f"{self.job_desc}",
+            )
+            return self._get_response_from_react(agent, prompt)
+        else:
+            agent_worker = FunctionCallingAgentWorker.from_tools(
+                [
+                    self.cv_vector_tool,
+                    self.job_vector_tool,
+                    self.cv_summary_tool,
+                    self.job_summary_tool,
+                ],
+                llm=self.llm,
+                verbose=True,
+            )
+            agent = ParallelAgentRunner(agent_worker)
+            response = agent.chat(prompt)
+            return str(response)
 
     def rag_task(self, task_type: str):
-        if task_type == "auto_cv":
-            return self._auto_rag(
-                "Tailor the user's CV (summary provided in cv_summary_tool and specific questions answered using cv_vector_tool) to be the best possible fit for the job description (summary provided in and specific questions answered using job_summary_tool and job_vector_tool). Do not invent information not in the user's CV."
-            )
-        elif task_type == "auto_cover_letter":
-            return self._auto_rag(
-                "Craft a cover letter using the user's resume (summary provided in cv_summary_tool and specific questions answered using cv_vector_tool) to demonstrate that the user is an ideal fit for the job (summary provided in job_summary_tool and specific questions answered using  and job_vector_tool). Do not invent information not in the user's CV."
-            )
-        elif task_type == "guided_cv":
-            return self._guided_rag(
-                "Tailor the user's CV to be the best possible fit for the job description. Do not invent information not in the user's CV."
-            )
-        elif task_type == "guided_cover_letter":
-            return self._guided_rag(
-                "Craft a cover letter using the user's CV to demonstrate that the user is an ideal fit for the job. Do not invent information not in the user's CV."
-            )
+        prompt = ""
+        if "cv" in task_type:
+            prompt = "Tailor the user's CV to be the best possible fit for the job description. Do not invent information not in the user's CV."
+        elif "cover_letter" in task_type:
+            prompt = "Craft a cover letter using the user's resume to demonstrate that the user is an ideal fit for the job. Do not invent information not in the user's CV."
+        else:
+            raise Exception("Invalid task type")
+
+        if "react" in task_type:
+            prompt + "\nJob description is provided in context."
+
+        if task_type.startswith("guided"):
+            return self._guided_rag(prompt, "react" in task_type)
+        elif task_type.startswith("auto"):
+            return self._auto_rag(prompt, "react" in task_type)
         else:
             raise Exception("Invalid task type")
